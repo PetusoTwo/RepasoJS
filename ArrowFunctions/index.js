144dd97c9a6164376173dc700c9b729aa84dceca
PetusoTwo <pedrocespedes456@gmail.com>
2024-08-17 12:17:57 -0500

    Arrow Functions

--- ArrowFunctions/index.js
+++ ArrowFunctions/index.js
@@ -8,6 +8,16 @@
 
 //En las funciones de flecha no es necesario el return, ya que automaticamente retornara.
 
-let saludar = (nombre) => {
-    return `Hola ${nombre}`;
+let saludar = nombre => {
+    return `Hola ${nombre}`; //no es necesario poner el return
 }
+
+let viajando = (destino, duracion) => {
+    return `Viajando a ${destino} por ${duracion} dias`
+}
+
+let saludando = saludar("Cristian"); //si retorna un valor debemos crear una variable y llamar a la funcion.
+console.log(saludando);
+
+let viaje = viajando("Mexico", 5);
+console.log(viaje);
