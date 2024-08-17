7b1dc4f308b883886ec836bb6a8c8c124f0cda48
PetusoTwo <pedrocespedes456@gmail.com>
2024-08-17 12:01:06 -0500

    Arrow Functions

---
+++ ArrowFunctions/index.js
@@ -0,0 +1,13 @@
+// ARROW FUNCTIONS
+
+// SINTAXIS 
+
+//let <mombre> = (<prametros>) => {
+//  Logica de la funcion    
+//};
+
+//En las funciones de flecha no es necesario el return, ya que automaticamente retornara.
+
+let saludar = (nombre) => {
+    return `Hola ${nombre}`;
+}
